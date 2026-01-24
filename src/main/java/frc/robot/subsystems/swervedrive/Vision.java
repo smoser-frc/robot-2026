e89dcba9165d1a0b94357300833264c6e42dfcac
joeyshibata <joseph.shibata@gmail.com>
2026-01-24 16:51:02 -0500

    2026 apritags working on two cameras

--- src/main/java/frc/robot/subsystems/swervedrive/Vision.java
+++ src/main/java/frc/robot/subsystems/swervedrive/Vision.java
@@ -48,7 +48,7 @@ public class Vision {
 
   /** April Tag Field Layout of the year. */
   public static final AprilTagFieldLayout fieldLayout =
-      AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeAndyMark);
+      AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltWelded);
 
   /** Ambiguity defined as a value between (0,1). Used in {@link Vision#filterPose}. */
   private final double maximumAmbiguity = 0.25;
@@ -291,27 +291,26 @@ public class Vision {
             Units.inchesToMeters(-12), Units.inchesToMeters(0), Units.inchesToMeters(0)),
         VecBuilder.fill(4, 4, 8),
         VecBuilder.fill(0.5, 0.5, 1)),
-  // /** Right Camera */
-  // RIGHT_CAM(
-  //     "right",
-  //     new Rotation3d(0, Math.toRadians(-24.094), Math.toRadians(-30)),
-  //     new Translation3d(
-  //         Units.inchesToMeters(12.056),
-  //         Units.inchesToMeters(-10.981),
-  //         Units.inchesToMeters(8.44)),
-  //     VecBuilder.fill(4, 4, 8),
-  //     VecBuilder.fill(0.5, 0.5, 1)),
-  // /** Center Camera */
-  // CENTER_CAM(
-  //     "center",
-  //     new Rotation3d(0, Units.degreesToRadians(18), 0),
-  //     new Translation3d(
-  //         Units.inchesToMeters(-4.628),
-  //         Units.inchesToMeters(-10.687),
-  //         Units.inchesToMeters(16.129)),
-  //     VecBuilder.fill(4, 4, 8),
-  //     VecBuilder.fill(0.5, 0.5, 1))
-  ;
+    /** Right Camera */
+    // RIGHT_CAM(
+    //     "right",
+    //     new Rotation3d(0, Math.toRadians(-24.094), Math.toRadians(-30)),
+    //     new Translation3d(
+    //         Units.inchesToMeters(12.056),
+    //         Units.inchesToMeters(-10.981),
+    //         Units.inchesToMeters(8.44)),
+    //     VecBuilder.fill(4, 4, 8),
+    //     VecBuilder.fill(0.5, 0.5, 1)),
+    /** Center Camera */
+    CENTER_CAM(
+        "camB",
+        new Rotation3d(0, Units.degreesToRadians(18), 0),
+        new Translation3d(
+            Units.inchesToMeters(-4.628),
+            Units.inchesToMeters(-10.687),
+            Units.inchesToMeters(16.129)),
+        VecBuilder.fill(4, 4, 8),
+        VecBuilder.fill(0.5, 0.5, 1));
 
     /** Latency alert to use when high latency is detected. */
     public final Alert latencyAlert;
