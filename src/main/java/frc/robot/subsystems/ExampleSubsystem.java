f2562e1e3c7a8bd5f5a2b7a69b45e7ebd72cfebf
Scott Moser <smoser@brickies.net>
2026-02-11 07:54:10 -0500

    Create a SmartMotorControllerConfig

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -4,10 +4,43 @@
 
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Seconds;
+
+import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import yams.mechanisms.SmartMechanism;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
 
 public class ExampleSubsystem extends SubsystemBase {
+
+  private SmartMotorControllerConfig smcConfig = new SmartMotorControllerConfig(this)
+  .withControlMode(ControlMode.CLOSED_LOOP)
+  // Feedback Constants (PID Constants)
+  .withClosedLoopController(50, 0, 0, DegreesPerSecond.of(90), DegreesPerSecondPerSecond.of(45))
+  .withSimClosedLoopController(50, 0, 0, DegreesPerSecond.of(90), DegreesPerSecondPerSecond.of(45))
+  // Feedforward Constants
+  .withFeedforward(new ArmFeedforward(0, 0, 0))
+  .withSimFeedforward(new ArmFeedforward(0, 0, 0))
+  // Telemetry name and verbosity level
+  .withTelemetry("ArmMotor", TelemetryVerbosity.HIGH)
+  // Gearing from the motor rotor to final shaft.
+  // In this example GearBox.fromReductionStages(3,4) is the same as GearBox.fromStages("3:1","4:1") which corresponds to the gearbox attached to your motor.
+  // You could also use .withGearing(12) which does the same thing.
+  .withGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
+  // Motor properties to prevent over currenting.
+  .withMotorInverted(false)
+  .withIdleMode(MotorMode.BRAKE)
+  .withStatorCurrentLimit(Amps.of(40))
+  .withClosedLoopRampRate(Seconds.of(0.25))
+  .withOpenLoopRampRate(Seconds.of(0.25));
+
   /** Creates a new ExampleSubsystem. */
   public ExampleSubsystem() {}
 
@@ -45,3 +78,4 @@ public class ExampleSubsystem extends SubsystemBase {
     // This method will be called once per scheduler run during simulation
   }
 }
+
